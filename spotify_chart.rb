9b6e6b50c850f4210e0060103a6303b4a4c3e50b
Katie Hoffman <ktahoffman@gmail.com>
2014-09-19 15:40:49 -0400

    remove pry, merge with master

--- spotify_chart.rb
+++ spotify_chart.rb
@@ -1,26 +1,34 @@
-# require files/modules/libraries here
+require 'open-uri'
+require 'json'
 
-class ClassName
-  # attrs here
+class SpotifyChart
+  attr_reader :base_url
 
-  def get_url
-    # code get_url here
+  def initialize
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
-  def get_json
-    # code get_json here
+  def get_url(version, region)
+    base_url + version + "/" + region  + "/latest"
   end
 
-  def fetch_track_album_artist
-    # code fetch_track_album_artist here
+  def get_json(url)
+    JSON.load(open(url))
   end
 
-  def most_streamed
-    # code most_streamed here
+  def fetch_track_album_artist(json)
+    song = json["tracks"][0]
+    "'#{song["track_name"]}' by #{song["artist_name"]} from the album #{song["album_name"]}"
   end
 
-  def most_shared
-    # code most_shared here
+  def most_streamed(region)
+    json = get_json(get_url("most_streamed", region))
+    fetch_track_album_artist(json)
+  end
+
+  def most_shared(region)
+    json = get_json(get_url("most_shared", region))
+    fetch_track_album_artist(json)
   end
 
 end
